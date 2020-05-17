74a616451c4761db146f08b67e5a52051bc92ee1
Ryan Dick <rdick@ualberta.ca>
2020-05-16 22:54:18 -0700

    Added the cart Reducer -> displays total cost and items in Cart

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
+import { AppLoading } from 'expo'
+import * as Font from 'expo-font'
+
 import productsReducer from './store/reducers/products'
+import cartReducer from './store/reducers/cart'
 import ShopNavigator from './navigation/ShopNavigator'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 const rootReducer = combineReducers({
-  products: productsReducer
+  products: productsReducer,
+  cart: cartReducer
 })
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, composeWithDevTools)
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  })
+}
 
 export default function App() {
-  return (
-    <Provider store={store}>
-      <ShopNavigator />
-    </Provider>
-  );
+  const [fontLoaded, setFontLoded] = useState(false)
+
+  if (!fontLoaded) {
+    return <AppLoading startAsync={fetchFonts} onFinish={() => { setFontLoded(true) }} />
+  } else {
+    return (
+      <Provider store={store}>
+        <ShopNavigator />
+      </Provider>
+    )
+  }
 }
--- screeens/shop/ProductsOverviewScreen.js
+++ screeens/shop/ProductsOverviewScreen.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { FlatList, Text, Image } from 'react-native'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import ProductItem from '../../components/shop/ProductItem'
+import * as cartActions from '../../store/actions/cart'
 
 const ProductOverviewScreen = props => {
     const products = useSelector(state => state.products.availableProducts)
+    const dispatch = useDispatch()
+
     return (
         <FlatList
             data={products}
@@ -15,7 +18,9 @@ const ProductOverviewScreen = props => {
                     title={itemData.item.title}
                     price={itemData.item.price}
                     onViewDetails={() => { props.navigation.navigate('ProductDetail', { productId: itemData.item.id, productTitle: itemData.item.title }) }}
-                    onViewCart={() => { }}
+                    onAddToCart={() => {
+                        dispatch(cartActions.addToCart(itemData.item))
+                    }}
                 />
             )}
         />
